5882db6cdace4a1afa4d4f900934e06485d045cf
Peter Le <105581900+petaa1@users.noreply.github.com>
2025-03-26 23:52:30 +0800

    init DonutChart

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import DonutChart from "./components/DonutChart";
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +30,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div className="flex flex-col items-center">
+        <h2 className="text-xl mt-4">Spending Breakdown</h2>
+        <DonutChart />
+      </div>
     </>
   )
 }
---
+++ client/src/components/DonutChart.tsx
@@ -0,0 +1,56 @@
+import { PieChart, Pie, Cell } from "recharts";
+
+interface Category {
+  name: string;
+  value: number;
+}
+
+const data: Category[] = [
+  { name: "Food", value: 400 },
+  { name: "Rent", value: 300 },
+  { name: "Entertainment", value: 300 },
+  { name: "Transportation", value: 200 },
+  { name: "Utilities", value: 100 },
+]
+
+const COLOURS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#8884d8"];
+
+const renderCenteredText = (total: number) => {
+  return (
+    <text x="50%" y="50%"
+    textAnchor="middle"
+    dominantBaseline="middle"
+    fontSize="20px"
+    fontWeight="bold"
+    fill="#FFFFFF">
+      {total}
+    </text>
+
+  )
+}
+
+const DonutChart: React.FC = () => {
+  const totalSpending = data.reduce((sum, item) => sum + item.value, 0);
+
+  return (
+    <PieChart width={400} height={400}>
+      <Pie
+        data={data}
+        cx="50%"
+        cy="50%"
+        innerRadius={60}
+        outerRadius={80}
+        fill="#8884d8"
+        dataKey="value"
+        label
+      >
+        {data.map((_,index) => (
+          <Cell key={`cell-${index}`} fill={COLOURS[index % COLOURS.length]} />
+        ))}
+      </Pie>
+      {renderCenteredText(totalSpending)}
+    </PieChart>
+  );
+};
+
+export default DonutChart;
